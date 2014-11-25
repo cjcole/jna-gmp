b2fe309d67e4cf698af10a67e3657a6d0bca6f87
Chris Cole <cjcole@alum.mit.edu>
2014-11-24 22:20:07 -0500

    Checkpointing.

--- example.clj
+++ example.clj
@@ -1,19 +1,23 @@
 (defn mpz [x] (new org.cjcole.jnagmp.Mpz x))
+
+(def np 2)
+(def p (mpz 47))
+(def q (.cdivQ (.sub p 1) 2))
+(def g (mpz 3)) ; call "prtable" to find possible values
+
 (def mpz0 (mpz 0))
 (def mpz+1 (fn [x] (.add x (mpz 1))))
 (defn mpz+ 
   ([] (mpz 0))
   ([a b] (.mod (.add a b) p)))
 (defn mpz*
+  ([] (mpz 1))
+  ([a b] (.mod (.mul a b) p)))
+(defn encr*
   ([] [(mpz 1) (mpz 1)])
   ([a b] [(.mod (.mul (first a) (first b)) p) (.mod (.mul (second a) (second b)) p)]))
 
 (require '[clojure.core.reducers :as r])
-
-(def np 1)
-(def p (mpz 47))
-(def q (.cdivQ (.sub p 1) 2))
-(def g (mpz 3)) ; call "prtable" to find possible values
 
 (defn prgroup []
   (dotimes [i (.toInt q)] (println "i =" i ", g^i =" (.powm g (mpz i) p))))
@@ -54,7 +58,6 @@
     (if (= i np)
       mvec
       (recur (+ i 1) (conj mvec (.add (mpz 1) (mpz i)))))))
-;; TODO: remove x
 (def x (.mod (r/fold mpz+ xvec) p))
 (def y (.powm g x p)) ; a.k.a. "h"
 
@@ -66,13 +69,17 @@
 (defn group-elem [m] (.powm g m p))
 (defn group-index [gm] (group-index-map gm))
 
-(defn encr1 [vec]
-  (let [m (nth vec 0) r (nth vec 1)]
+(defn encr 
+  ([mvec] 
+    (defn encr-helper [vec]
+      (let [m (nth vec 0) r (nth vec 1)]
+        (encr m r)))
+     (r/fold encr* (map encr-helper (map vector mvec rands))))
+  ([m r] 
     (let [result [(.powm g r p) (.mod (.mul (group-elem m) (.powm y r p)) p)]]
 ;      (println "m =" m ", y =" y ", r =" r ", result =" result)
 ;      (println "  gm =" (group-elem m) ", (.powm y r p) =" (.powm y r p))
       result)))
-(defn encr [mvec] (r/fold mpz* (map encr1 (map vector mvec rands))))
 (defn decr [e] (group-index (.mod (.mul (second e) (.powm (first e) (.neg x) p)) p)))
 
 ; (def rands [(mpz 2) (mpz 4) (mpz 6)])
@@ -94,6 +101,20 @@
             q1 (.powm g2 y p)
             q2 (.mod (.mul (.powm h2 c p) b) p)]
         (and (= p1 p2) (= q1 q2))))))
+
+(defn joint-oblivious-eq? [e1 e2]
+  (let [a (mpz 8) ; random from Z*q
+        m (.mod (.mul (first e1) (.invert (first e2) p)) p)
+        s (.mod (.mul (second e1) (.invert (second e2) p)) p)
+        sa (.powm s a p) ; s^a
+        ma (.powm m a p)] ; m^a
+    (defn make-max [xi]
+      (.powm m (.mul a xi) p)) ; m^(a*x)
+    (let [max (r/fold mpz* (map make-max xvec))]
+      (println sa)
+      (println max)
+      (println ma)
+      (= sa max)))) ; check logs
 
 (defn oblivious-eq? [e1 e2]
   (let [
@@ -117,19 +138,24 @@
 (oblivious-eq? e esame)
 (oblivious-eq? e ediff)
 
-(dotimes [n1 (.toInt p)]
-  (let [n2 (decr (encr1 [(mpz n1) r1]))]
-    (if (not (= (mpz n1) n2))
-      (println (str (format "%2d %2d" n1 (.toInt n2)))))))
+(dotimes [r (.toInt q)]
+  (dotimes [in (.toInt q)]
+    (let [out (.toInt (decr (encr (mpz in) (mpz r))))]
+      (if (not (= in out))
+        (println (str (format "in=%2d, out=%2d, r=%2d" in out r)))))))
 
-(dotimes [n1 (.toInt q)] 
-  (dotimes [n2 (.toInt q)]
-    (let [
-          e1 (encr1 [(mpz (.powm g (mpz n1) p)) r1])
-          e2 (encr1 [(mpz (.powm g (mpz n2) p)) r2])
-         ]
-      (if (or
-            (and (= n1 n2) (not (oblivious-eq? e1 e2)))
-            (and (not (= n1 n2)) (oblivious-eq? e1 e2)))
-        (println (str (format "%2d %2d %s" n1 n2
-          (if (oblivious-eq? e1 e2) "true" "false"))))))))
+(dotimes [r1 (.toInt q)]
+  ; (println r1)
+  (dotimes [r2 0] ; (.toInt q)]
+    (dotimes [m1 (.toInt q)] 
+      (dotimes [m2 (.toInt q)]
+        (let [
+              e1 (encr (mpz m1) (mpz r1))
+              e2 (encr (mpz m2) (mpz r2))
+             ]
+          (if (or
+                (and (= m1 m2) (not (oblivious-eq? e1 e2)))
+                (and (not (= m1 m2)) (oblivious-eq? e1 e2)))
+            (println (str (format "%2d %2d %s" m1 m2
+              (if (oblivious-eq? e1 e2) "true" "false"))))))))))
+ 
